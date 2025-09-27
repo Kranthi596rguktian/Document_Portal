ae96cb6d5afb3e91ffc2df45d942b6b1abab6003
Kranthi596rguktian <kranthimutyalapalli596@gmail.com>
2025-09-27 20:41:56 +0530

    model loader code updated

--- README.md
+++ README.md
@@ -1,15 +1,67 @@
-# Minimum Requirement for this project
+# Project Setup Guide
 
+## Create Project Folder and Environment Setup
 
-1. LLM Model   ##goq(freely), openai, gemini, claude, huggingface(freely) and ollama(local setup)
+```bash
+# Create a new project folder
+mkdir <project_folder_name>
 
-2. Embedding model ## openai, hf and gemini
+# Move into the project folder
+cd <project_folder_name>
 
-3. Vector Database  
-    a. in-memory
-    b. on-disk
-    c. cloud-based
+# Open the folder in VS Code
+code .
 
+# Create a new Conda environment with Python 3.10
+conda create -p <env_name> python=3.10 -y
 
-GroQ API KEY : https://console.groq.com/keys
-Gemini API KEY : https://aistudio.google.com/apikey
+# Activate the environment (use full path to the environment)
+conda activate <path_of_the_env>
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Initialize Git
+git init
+
+# Stage all files
+git add .
+
+# Commit changes
+git commit -m "<write your commit message>"
+
+# Push to remote (after adding remote origin)
+git push
+
+# Cloning the repository
+git clone https://github.com/sunnysavita10/document_portal.git
+```
+## Minimum Requirements for the Project
+
+### LLM Models
+- **Groq** (Free)
+- **OpenAI** (Paid)
+- **Gemini** (15 Days Free Access)
+- **Claude** (Paid)
+- **Hugging Face** (Free)
+- **Ollama** (Local Setup)
+
+### Embedding Models
+- **OpenAI**
+- **Hugging Face**
+- **Gemini**
+
+### Vector Databases
+- **In-Memory**
+- **On-Disk**
+- **Cloud-Based**
+
+## API Keys
+
+### GROQ API Key
+- [Get your API Key](https://console.groq.com/keys)  
+- [Groq Documentation](https://console.groq.com/docs/overview)
+
+### Gemini API Key
+- [Get your API Key](https://aistudio.google.com/apikey)  
+- [Gemini Documentation](https://ai.google.dev/gemini-api/docs/models)
---
+++ utils/model_loader.py
@@ -0,0 +1,85 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+
+from utils.config_loader import load_config
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+log = CustomLogger().get_logger(__name__)
+
+class ModelLoader:
+    """Handles the loading of various models and configurations."""
+
+    def __init__(self):
+        """Initialize the ModelLoader with environment variables and configurations."""
+        load_dotenv()
+        self._validate_env()
+        self.config = load_config()
+        log.info("Configuration loaded successfully.", config_key = list(self.config.keys()))
+
+    def _validate_env(self):
+        """Validate environment variables."""
+        required_vars = ["GOOGLE_API_KEY", "GROQ_API_KEY"]
+        self.api_keys = {key: os.getenv(key) for key in required_vars}
+        missing_vars = [key for key, value in self.api_keys.items() if not value]
+        if missing_vars:
+            log.error("Missing environment variables: %s", missing_vars)
+            raise DocumentPortalException("Missing environment variables",sys)
+
+    def load_embeddings(self):
+        try:
+            log.info("Loading Google Generative AI Embeddings model.")
+            model_name = self.config["embedding"]['model_name']
+            return GoogleGenerativeAIEmbeddings(model=model_name)
+        except Exception as e:
+            log.error("Error loading Embeddings model: %s", e)
+            raise DocumentPortalException("Error loading Embeddings model",sys)
+
+    def load_llm(self):
+        """
+        Load and return the LLM model.
+        Load LLM dynamically based on provider in config."""
+
+        llm_block = self.config.get("llm", {})
+        log.info("Loading LLM model from config: %s", llm_block)
+        provider_key = os.getenv("LLM_PROVIDER", "groq")
+
+        if provider_key not in llm_block:
+            log.error("Unsupported LLM provider: %s", provider_key)
+            raise DocumentPortalException("Unsupported LLM provider",sys)
+        
+        llm_config = llm_block[provider_key]
+        provider = llm_config.get("provider")
+        model_name = llm_config.get("model_name")
+        temperature = llm_config.get("temperature", 0)
+        max_tokens = llm_config.get("max_tokens", 2048)
+
+        log.info("Loading LLM model", provider_key=provider_key, model_name=model_name, temperature=temperature, max_tokens=max_tokens)
+
+        if provider_key == "groq":
+            llm = ChatGroq(model=model_name, temperature=temperature, max_tokens=max_tokens, api_key=self.api_keys["GROQ_API_KEY"])
+            return llm
+        elif provider_key == "google":
+            llm = ChatGoogleGenerativeAI(model=model_name, temperature=temperature, max_tokens=max_tokens, api_key=self.api_keys["GOOGLE_API_KEY"])
+            return llm
+        else:
+            log.error("Unsupported LLM provider: %s", provider_key)
+            raise DocumentPortalException("Unsupported LLM provider",sys)
+
+if __name__ == "__main__":
+    loader = ModelLoader()
+
+    embeddings = loader.load_embeddings()
+    print("Embeddings model loaded:", embeddings)
+
+    llm = loader.load_llm()
+    print("LLM model loaded:", llm)
+
+    #Test the modelloader
+    result = llm.invoke(["Hello, how are you?"])
+    print("LLM model invocation result:", result.content)
