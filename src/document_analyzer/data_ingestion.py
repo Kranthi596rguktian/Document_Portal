fa33d51eab26ae1441cbb20b16e15dee02206804
Kranthi596rguktian <kranthimutyalapalli596@gmail.com>
2025-09-27 21:34:12 +0530

    removed API Key

--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -11,12 +11,26 @@ class DocumentHandler:
     Automatically logs all actions and supports session-based organization.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self,data_dir=None,session_id=None):
+
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+            self.data_dir = data_dir or os.getenv(
+                "DATA_STORAGE_PATH",
+                os.path.join(os.getcwd(),"data","document_analysis")
+            )
+            self.session_id = session_id or f"session_{datetime.now().strftime('%Y%m%d_%H%M%S')}_{str(uuid.uuid4())[:8]}"
+
+            self.session_path = os.path.join(self.data_dir, self.session_id)
+            os.makedirs(self.session_path, exist_ok=True)
+            self.log.info("PDF Handler initialized.",session_id=self.session_id,session_path=self.session_path)
+
+        except Exception as e:
+            self.log.error("Error initializing PDF Handler.", error=str(e))
+            raise DocumentPortalException("Failed to initialize PDF Handler.", e) from e
 
     def save_pdf(self):
         pass
 
     def read_pdf(self):
         pass
-    
