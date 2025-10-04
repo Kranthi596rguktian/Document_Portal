4eaa13464e37407863b0ae6199425ef6375f445c
Kranthi596rguktian <kranthimutyalapalli596@gmail.com>
2025-10-04 13:38:19 +0530

    code skeleton updated for multidoc

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,11 @@
+class DocumentIngestor:
+    def __init(self):
+        pass
+    
+    def ingest_file(self):
+        pass
+    
+    def _create_retriever(self,documents):
+        pass
+    
+    
